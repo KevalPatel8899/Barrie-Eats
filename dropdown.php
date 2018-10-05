72ac6b056d6c1105006020d74339e39024994ffd
KevalPatel8899 <200393482@student.georgianc.on.ca>
2018-10-05 09:01:30 -0400

    Barrie Eats

---
+++ dropdown.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dropdown Test</title>
+</head>
+<body>
+<?php
+// connect
+$db = new PDO('mysql:host=localhost;dbname=barrieEats', 'root', '');
+// set up query
+$sql = "SELECT name FROM restaurants ORDER BY name";
+// execute & store the result
+$cmd = $db->prepare($sql);
+$cmd->execute();
+$restaurants = $cmd->fetchAll();
+// create dropdown list
+echo '<select name="name">';
+foreach ($restaurants as $r) {
+    echo '<option>' . $r['name'] . '</option>';
+}
+// close list
+echo '</select>';
+// disconnect
+$db = null;
+?>
+</body>
+</html>
